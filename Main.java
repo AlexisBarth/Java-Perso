73985dcb534f79f160dbf3cd8f64ccb51065efd2
Alexis <alexis.barthelmebs@epitech.eu>
2020-11-20 19:28:07 +0100

    Init
    First Comit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    int x = 5;
+    int y = 6;
+    String name = "toutou";
+  
+    public static void main(String[] args) {
+      Main myObj = new Main();
+      System.out.println(myObj.name);
+    }
+  }
+
+
